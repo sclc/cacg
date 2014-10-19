addc87b95a3161dfdc764dc9ae146470124ec928
sclc <sclc@sclcs-MacBook-Pro.local>
2014-10-19 13:46:49 +0900

    for tsqr kernel

---
+++ src/TSQR.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   TSQR.h
+ * Author: scl
+ *
+ * Created on September 7, 2014, 7:40 PM
+ */
+
+#ifndef TSQR_H
+#define	TSQR_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TSQR_H */
+
